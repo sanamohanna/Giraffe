8a76eebab4927c604cf838389816fc3eac6ab84e
sanamohanna <sanamohanna2305@gmail.com>
2022-11-20 20:52:04 +0200

    Update Question.java

--- Knight'sMove/src/model/Question.java
+++ Knight'sMove/src/model/Question.java
@@ -3,6 +3,7 @@ package model;
 import java.util.ArrayList;
 import java.util.Objects;
 import Enum.DifficultyLevel;
+
 public class Question {
 
 	private int id;
@@ -11,36 +12,29 @@ public class Question {
 	private int corect_ans;
 	private DifficultyLevel difficultyLevel;
 	private String team;
+
 	public Question(int id, String context, ArrayList<Answer> answers, DifficultyLevel level, String team) {
 		super();
 		this.id = id;
 		Context = context;
 		this.answers = answers;
-		this.corect_ans=this.getCorrectAnswer();
+		this.corect_ans = this.getCorrectAnswer();
 		this.difficultyLevel = level;
 		this.team = team;
 	}
-	
-
 
 	public int getCorect_ans() {
 		return this.getCorrectAnswer();
 	}
 
-
-
 	public void setCorect_ans(int corect_ans) {
 		this.corect_ans = corect_ans;
 	}
-
-
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(Context, answers, difficultyLevel, id, team);
 	}
-
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -55,75 +49,82 @@ public class Question {
 				&& difficultyLevel == other.difficultyLevel && id == other.id && Objects.equals(team, other.team);
 	}
 
-
-
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getContext() {
 		return Context;
 	}
+
 	public void setContext(String context) {
 		Context = context;
 	}
+
 	public ArrayList<Answer> getAnswers() {
 		return answers;
 	}
+
 	public void setAnswers(ArrayList<Answer> answers) {
 		this.answers = answers;
 	}
+
 	public DifficultyLevel getDifficultyLevel() {
 		return difficultyLevel;
 	}
+
 	public void setDifficultyLevel(DifficultyLevel level) {
 		this.difficultyLevel = level;
 	}
+
 	public String getTeam() {
 		return team;
 	}
+
 	public void setTeam(String team) {
 		this.team = team;
 	}
+
 	public void updateAnswers(ArrayList<Answer> updatedAnswers) {
 		this.answers = new ArrayList<Answer>();
 
-		for(Answer a : updatedAnswers) {
+		for (Answer a : updatedAnswers) {
 			this.addAnswer(a);
 		}
 	}
 
 	public int getCorrectAnswer() {
-		for(int i= 0; i< answers.size();i++) {
-			if(answers.get(i).isTrue()) {
+		for (int i = 0; i < answers.size(); i++) {
+			if (answers.get(i).isTrue()) {
 				return answers.get(i).getId();
 			}
 		}
 		return -1;
 	}
-	
+
 	public Boolean addAnswer(Answer answer) {
-		if(answer != null) {
+		if (answer != null) {
 			return this.answers.add(answer);
 		}
 		return false;
 	}
-	
+
 	public Boolean removeAnswer(Answer answer) {
-		if(answer != null) {
+		if (answer != null) {
 			return this.answers.remove(answer);
 		}
 		return false;
 
 	}
 
-
-
 	@Override
 	public String toString() {
-		return "Question [id=" + id + ", Context=" + Context + ", answers=" + answers +  ", difficultyLevel=" + difficultyLevel + ", team=" + team + "]";
+		return "Question [id=" + id + ", Context=" + Context + ", answers=" + answers + ", difficultyLevel="
+				+ difficultyLevel + ", team=" + team + "]";
 	}
-	
+
 }
