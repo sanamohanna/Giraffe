c7fd0e6d06a8bc4c31dd564f87718a3a56188942
sanamohanna <sanamohanna2305@gmail.com>
2022-11-20 20:51:51 +0200

    Update Piece.java

--- Knight'sMove/src/model/Piece.java
+++ Knight'sMove/src/model/Piece.java
@@ -5,25 +5,21 @@ import java.util.ArrayList;
 public class Piece {
 
 	private ArrayList<Integer> place;
-	
-	
+
 	public Piece(ArrayList<Integer> place) {
 		super();
 		this.place = place;
 	}
 
-
 	public ArrayList<Integer> getPlace() {
 		return place;
 	}
-
 
 	public void setPlace(ArrayList<Integer> place) {
 		this.place = place;
 	}
 
-
 	public void straightMove() {
 	}
-	
+
 }
