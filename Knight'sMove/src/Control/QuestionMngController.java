a7b9c026cd35c5c1c56a1fad2ce4fa9bf7ee42f3
sanamohanna <sanamohanna2305@gmail.com>
2022-11-20 20:51:06 +0200

    Update QuestionMngController.java

--- Knight'sMove/src/Control/QuestionMngController.java
+++ Knight'sMove/src/Control/QuestionMngController.java
@@ -14,8 +14,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
-
 import org.json.*;
 
 import org.json.simple.*;
@@ -24,42 +22,39 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
-import com.jn.easyjson.core.tree.JsonIOException;
 
 import Enum.DifficultyLevel;
 import model.Answer;
 import model.Question;
 import model.SysData;
 
-
 public class QuestionMngController {
 	private SysData sysData;
 	private static QuestionMngController instance;
-	
+
 	public QuestionMngController() {
-		
-		//this.questionScreen = new ManageQuestions();
+
+		// this.questionScreen = new ManageQuestions();
 		this.sysData = SysData.getInstance();
-		
-		
+
 	}
-	
 
 	/**
 	 * Instance Getter Of QuestionMgmtController
+	 * 
 	 * @return instance
 	 */
-	public static QuestionMngController getInstance() 
-	{ 
-		if (instance == null) 
-		{ 
-			instance = new QuestionMngController(); 
-		} 
-		return instance; 
+	public static QuestionMngController getInstance() {
+		if (instance == null) {
+			instance = new QuestionMngController();
+		}
+		return instance;
 	}
+
 	/**
 	 * SysData Instance Getter
 	 */
@@ -90,7 +85,7 @@ public class QuestionMngController {
 				answerArray.add(a.getAnswerContext());
 
 			}
-			
+
 			int difficulty = 0;
 			if (q.getDifficultyLevel().equals(DifficultyLevel.EASY)) {
 				difficulty = 1;
@@ -130,175 +125,165 @@ public class QuestionMngController {
 		}
 
 	}
-	
-		/**
-		 * Load Trivia Questions From JSON File
-		 */
-		public void LoadQuestions() {
 
-			ArrayList<Question> questions = new ArrayList<Question>();
-			if(this.getSysData().getQuestions()!= null)
-				this.getSysData().getQuestions().clear();
-			
-			Gson gson = new Gson();
-			JsonReader reader = null;
-			try {
-				reader = new JsonReader(new FileReader("src/QuestionsAndAnswers.json"));
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+	/**
+	 * Load Trivia Questions From JSON File
+	 */
+	public void LoadQuestions() {
+
+		ArrayList<Question> questions = new ArrayList<Question>();
+		if (this.getSysData().getQuestions() != null)
+			this.getSysData().getQuestions().clear();
+
+		Gson gson = new Gson();
+		JsonReader reader = null;
+		try {
+			reader = new JsonReader(new FileReader("src/QuestionsAndAnswers.json"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		@SuppressWarnings("deprecation")
+		JsonObject jsonObject = new JsonParser().parse(reader).getAsJsonObject();
+
+		final JsonArray data = jsonObject.getAsJsonArray("questions");
+
+		for (JsonElement element : data) {
+
+			Question q;
+
+			String context = ((JsonObject) element).get("question").getAsString();
+
+			System.out.println(context);
+			JsonArray answersArray = (((JsonObject) element).getAsJsonArray("answers"));
+
+			@SuppressWarnings("unchecked")
+			ArrayList<String> answers = gson.fromJson(answersArray, ArrayList.class);
+			for (String ans : answers) {
+				System.out.println(ans);
 			}
-			
-			@SuppressWarnings("deprecation")
-			JsonObject jsonObject = new JsonParser().parse(reader).getAsJsonObject();
-			
-			final JsonArray data = jsonObject.getAsJsonArray("questions");
-			
+			int correct = ((JsonObject) element).get("correct_ans").getAsInt();
+			System.out.println(correct);
+			int difficulty = ((JsonObject) element).get("level").getAsInt();
+			System.out.println(difficulty);
+			String team = ((JsonObject) element).get("team").getAsString();
+			System.out.println(team);
 
-			for (JsonElement element : data) {
+			if (!this.sysData.getQuestions().isEmpty()) {
+				q = new Question(this.sysData.getQuestions().size(), context, new ArrayList<Answer>(), null, team);
+				this.sysData.getQuestions().add(q);
 
-				Question q;
+			} else {
+				System.out.println("y");
+				q = new Question(0, context, new ArrayList<Answer>(), null, team);
+				this.sysData.getQuestions().add(q);
 
-				String context = ((JsonObject) element).get("question").getAsString();
+			}
 
-				System.out.println(context);
-				JsonArray answersArray = (((JsonObject) element).getAsJsonArray("answers"));
-				
-				
-				@SuppressWarnings("unchecked")
-				ArrayList<String> answers = gson.fromJson(answersArray, ArrayList.class);
-				for(String ans :answers ) {
-					System.out.println(ans);
-				}
-				int correct = ((JsonObject) element).get("correct_ans").getAsInt();
-				System.out.println(correct);
-				int difficulty = ((JsonObject) element).get("level").getAsInt();
-				System.out.println(difficulty);
-				String team = ((JsonObject) element).get("team").getAsString();
-				System.out.println(team);
-			
-				if ( !this.sysData.getQuestions().isEmpty()) {
-					q = new Question(this.sysData.getQuestions().size(), context,new ArrayList<Answer>(), null, team);
-					this.sysData.getQuestions().add(q);
-						
-					}
-				else
-					{
-					System.out.println("y");
-					q = new Question(0, context,  new ArrayList<Answer>(), null, team);
-					this.sysData.getQuestions().add(q);
-					
-				}
-				
-				DifficultyLevel diff_level;
-				if (difficulty == 1) {
-					diff_level = DifficultyLevel.EASY;
-				} else if (difficulty == 2) {
-					diff_level = DifficultyLevel.MEDIOCRE;
-				} else {
-					diff_level = DifficultyLevel.HARD;
-				}
+			DifficultyLevel diff_level;
+			if (difficulty == 1) {
+				diff_level = DifficultyLevel.EASY;
+			} else if (difficulty == 2) {
+				diff_level = DifficultyLevel.MEDIOCRE;
+			} else {
+				diff_level = DifficultyLevel.HARD;
+			}
 
-				q.setDifficultyLevel(diff_level);
-				
+			q.setDifficultyLevel(diff_level);
 
-				int correctAnswer_Checker = 0;
-				
-				
-				if(answers.size() < 2) {
-					continue;
-				}
-				
-				boolean foundMatching = false;
-				for(int i = 0; i < answers.size(); i ++) {
-					
-					for(int j = 0; j < answers.size(); j++) {
-						if( i != j) {
-							
-							if(answers.get(i).equals(answers.get(j))) {
-								
-								foundMatching = true;
-								break;
-								
-							}
+			int correctAnswer_Checker = 0;
+
+			if (answers.size() < 2) {
+				continue;
+			}
+
+			boolean foundMatching = false;
+			for (int i = 0; i < answers.size(); i++) {
+
+				for (int j = 0; j < answers.size(); j++) {
+					if (i != j) {
+
+						if (answers.get(i).equals(answers.get(j))) {
+
+							foundMatching = true;
+							break;
 
 						}
-					
-					}
-					
-					if(foundMatching == true) {
-						break;
-					}
-					
-					
-				}
-				
-				if(foundMatching == true) {
-					continue;
-				}
 
-				for (String s : answers) {
-					correctAnswer_Checker++;
-					Answer a = null;
-					
-
-					if (correctAnswer_Checker == correct) {
-						a = new Answer(correctAnswer_Checker, s, true);
-					} else {
-						a = new Answer(correctAnswer_Checker, s, false);
 					}
-					q.addAnswer(a);
 
 				}
 
-				questions.add(q);
+				if (foundMatching == true) {
+					break;
+				}
 
 			}
-			
-			this.getSysData().setQuestions(questions);
-			for(int i=0;i<this.getSysData().getQuestions().size();i++) {
-				System.out.println(this.getSysData().getQuestions().get(i));
-				
+
+			if (foundMatching == true) {
+				continue;
 			}
-			
-			
+
+			for (String s : answers) {
+				correctAnswer_Checker++;
+				Answer a = null;
+
+				if (correctAnswer_Checker == correct) {
+					a = new Answer(correctAnswer_Checker, s, true);
+				} else {
+					a = new Answer(correctAnswer_Checker, s, false);
+				}
+				q.addAnswer(a);
+
+			}
+
+			questions.add(q);
 
 		}
-		
-		/**
-		 * Get Questions
-		 * @return - Questions From Model
-		 */
-		
-		public ArrayList<Question> getQuestions(){
-			return this.sysData.getQuestions();
+
+		this.getSysData().setQuestions(questions);
+		for (int i = 0; i < this.getSysData().getQuestions().size(); i++) {
+			System.out.println(this.getSysData().getQuestions().get(i));
+
 		}
-		
-		/**
-		 * removes question that had give id
-		 * @param id - question id
-		 */
-		
-		public void removeQuestions(int id){
-			 this.sysData.removeQuestion(id);
-		}
-		
-		/**
-		 * Checks if question already exists
-		 * @param content - question's content
-		 * @return true/false
-		 */
-		
-		public boolean quesAlreadyExists(String content) {
-			
-			for(Question q : this.getSysData().getQuestions()) {
-				if(q.getContext().equals(content)) {
-					return true;
-				}
+
+	}
+
+	/**
+	 * Get Questions
+	 * 
+	 * @return - Questions From Model
+	 */
+
+	public ArrayList<Question> getQuestions() {
+		return this.sysData.getQuestions();
+	}
+
+	/**
+	 * removes question that had give id
+	 * 
+	 * @param id - question id
+	 */
+
+	public void removeQuestions(int id) {
+		this.sysData.removeQuestion(id);
+	}
+
+	/**
+	 * Checks if question already exists
+	 * 
+	 * @param content - question's content
+	 * @return true/false
+	 */
+
+	public boolean quesAlreadyExists(String content) {
+
+		for (Question q : this.getSysData().getQuestions()) {
+			if (q.getContext().equals(content)) {
+				return true;
 			}
-			return false;
 		}
-		
-	
-	
-	
+		return false;
+	}
+
 }
