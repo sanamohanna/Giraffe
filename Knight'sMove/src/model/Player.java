1913255c5ac47caa74eb36f96c84ab95216f272e
sanamohanna <sanamohanna2305@gmail.com>
2022-11-20 20:51:56 +0200

    Update Player.java

--- Knight'sMove/src/model/Player.java
+++ Knight'sMove/src/model/Player.java
@@ -8,6 +8,7 @@ public class Player {
 	private int points;
 	private Knight knight;
 	private ArrayList<Game> gamesHistory;
+
 	public Player(String nickname, int points, Knight knight, ArrayList<Game> gamesHistory) {
 		super();
 		this.nickname = nickname;
@@ -15,31 +16,37 @@ public class Player {
 		this.knight = knight;
 		this.gamesHistory = gamesHistory;
 	}
+
 	public String getNickname() {
 		return nickname;
 	}
+
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
+
 	public int getPoints() {
 		return points;
 	}
+
 	public void setPoints(int points) {
 		this.points = points;
 	}
+
 	public Knight getKnight() {
 		return knight;
 	}
+
 	public void setKnight(Knight knight) {
 		this.knight = knight;
 	}
+
 	public ArrayList<Game> getGamesHistory() {
 		return gamesHistory;
 	}
+
 	public void setGamesHistory(ArrayList<Game> gamesHistory) {
 		this.gamesHistory = gamesHistory;
 	}
-	
-	
-	
+
 }
