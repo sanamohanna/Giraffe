fe43ba2c62ff5a18ecea06a0f37913cd557c8914
sanamohanna <sanamohanna2305@gmail.com>
2022-11-20 20:51:35 +0200

    Update King.java

--- Knight'sMove/src/model/King.java
+++ Knight'sMove/src/model/King.java
@@ -3,46 +3,34 @@ package model;
 import java.util.ArrayList;
 
 public class King extends Piece {
-  
+
 	private ArrayList<Integer> initialPlaceKing;
 	private int speed;
 
-	
-	
 	public King(ArrayList<Integer> place, ArrayList<Integer> initialPlaceKing, int speed) {
 		super(place);
 		this.initialPlaceKing = initialPlaceKing;
 		this.speed = speed;
 	}
 
-
-
 	public ArrayList<Integer> getInitialPlaceKing() {
 		return initialPlaceKing;
 	}
-
-
 
 	public void setInitialPlaceKing(ArrayList<Integer> initialPlaceKing) {
 		this.initialPlaceKing = initialPlaceKing;
 	}
 
-
-
 	public int getSpeed() {
 		return speed;
 	}
-
-
 
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
 
+	public void DiagonallyMove() {
 
-
-	public void  DiagonallyMove() {
-		
 	}
-	
+
 }
