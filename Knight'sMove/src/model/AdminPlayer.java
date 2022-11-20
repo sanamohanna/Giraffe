6af9ac7e524497567caff45c5e25018be9791b19
sanamohanna <sanamohanna2305@gmail.com>
2022-11-20 20:51:21 +0200

    Update AdminPlayer.java

--- Knight'sMove/src/model/AdminPlayer.java
+++ Knight'sMove/src/model/AdminPlayer.java
@@ -2,18 +2,16 @@ package model;
 
 import java.util.ArrayList;
 
-public class AdminPlayer extends Player{
-   private String password;
+public class AdminPlayer extends Player {
+	private String password;
 
+	public AdminPlayer(String nickname, int points, Knight knight, ArrayList<Game> gamesHistory, String password) {
+		super(nickname, points, knight, gamesHistory);
+		this.password = password;
+	}
 
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-public AdminPlayer(String nickname, int points, Knight knight, ArrayList<Game> gamesHistory, String password) {
-	super(nickname, points, knight, gamesHistory);
-	this.password = password;
-}
-
-public void setPassword(String password) {
-	this.password = password;
-}
-   
 }
