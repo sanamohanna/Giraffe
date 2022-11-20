3c8d166fe6a3f900da8cc092125aaf92bab9f86a
sanamohanna <sanamohanna2305@gmail.com>
2022-11-20 20:51:26 +0200

    Update Answer.java

--- Knight'sMove/src/model/Answer.java
+++ Knight'sMove/src/model/Answer.java
@@ -4,34 +4,41 @@ public class Answer {
 	private int id;
 	private String answerContext;
 	private boolean isTrue;
+
 	public Answer(int id, String answerContext, boolean isTrue) {
 		super();
 		this.id = id;
 		this.answerContext = answerContext;
 		this.isTrue = isTrue;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getAnswerContext() {
 		return answerContext;
 	}
+
 	public void setAnswerContext(String answerContext) {
 		this.answerContext = answerContext;
 	}
+
 	public boolean isTrue() {
 		return isTrue;
 	}
+
 	public void setTrue(boolean isTrue) {
 		this.isTrue = isTrue;
 	}
+
 	@Override
 	public String toString() {
 		return "Answer [id=" + id + ", answerContext=" + answerContext + ", isTrue=" + isTrue + "]";
 	}
-	
-	
+
 }
