d8c04464a7b7f38dc13284301c860875bd05464e
sanamohanna <sanamohanna2305@gmail.com>
2022-11-27 01:09:23 +0200

    Update Board.java

--- Knight'sMove/src/model/Board.java
+++ Knight'sMove/src/model/Board.java
@@ -3,4 +3,17 @@ package model;
 public class Board {
 	private char[][] boxes;
 
+	public Board(char[][] boxes) {
+		super();
+		this.boxes = boxes;
+	}
+
+	public char[][] getBoxes() {
+		return boxes;
+	}
+
+	public void setBoxes(char[][] boxes) {
+		this.boxes = boxes;
+	}
+
 }
