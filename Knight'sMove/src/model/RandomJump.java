f20bddc45480f6432b7c58a67948de2fe9c277a6
sanamohanna <sanamohanna2305@gmail.com>
2022-11-27 01:41:36 +0200

    Create RandomJump.java

---
+++ Knight'sMove/src/model/RandomJump.java
@@ -0,0 +1,15 @@
+package model;
+
+import Enum.Color;
+
+public class RandomJump extends Squares {
+
+	public RandomJump(Piece peice, Integer x, Integer y, Color color) {
+		super(peice, x, y, color);
+		}
+	
+	
+ public void changePlaceToEmptuSquare() {
+	 
+ }
+}
