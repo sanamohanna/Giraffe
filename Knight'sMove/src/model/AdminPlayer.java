6fd56ad1f60b31d016bddacc23ab664fadc3643a
sanamohanna <sanamohanna2305@gmail.com>
2022-11-27 01:09:12 +0200

    Update AdminPlayer.java

--- Knight'sMove/src/model/AdminPlayer.java
+++ Knight'sMove/src/model/AdminPlayer.java
@@ -9,6 +9,10 @@ public class AdminPlayer extends Player {
 		super(nickname, points, knight, gamesHistory);
 		this.password = password;
 	}
+    
+	public String getPassword() {
+		return password;
+	}
 
 	public void setPassword(String password) {
 		this.password = password;
