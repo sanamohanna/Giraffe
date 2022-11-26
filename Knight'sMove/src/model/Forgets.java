b1f62271cb2af4067e8b5b6f1c188e26b72fda03
sanamohanna <sanamohanna2305@gmail.com>
2022-11-27 01:41:33 +0200

    Create Forgets.java

---
+++ Knight'sMove/src/model/Forgets.java
@@ -0,0 +1,16 @@
+package model;
+
+import Enum.Color;
+
+public class Forgets  extends Squares{
+
+	public Forgets(Piece peice, Integer x, Integer y, Color color) {
+		super(peice, x, y, color);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void handleLastThreeSteps() {
+		
+	}
+
+}
