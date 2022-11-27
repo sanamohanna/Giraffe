e436c5eb124a02218a5e8e6d6ab9bd750e133e22
sanamohanna <sanamohanna2305@gmail.com>
2022-11-27 22:27:42 +0200

    Update Knight.java

--- Knight'sMove/src/model/Knight.java
+++ Knight'sMove/src/model/Knight.java
@@ -28,7 +28,39 @@ public class Knight extends Piece {
 	public void setKilled(boolean isKilled) {
 		this.isKilled = isKilled;
 	}
-	public void level1Move(){
+	public void level1Move( String direction1 , String direction2){
+		if(direction1 == "left" && direction2 == "up") {
+			this.LeftMove();
+			this.upMove();
+		}
+		if(direction1 == "left" && direction2 == "down") {
+			this.LeftMove();
+			this.downMove();
+		}
+        if(direction1 == "right" && direction2 == "up" ) {
+			this.RightMove();
+			this.upMove();
+		}
+        if(direction1 == "right" && direction2 == "down" ) {
+			this.RightMove();
+			this.downMove();
+		}
+        if(direction1 == "up" && direction2 == "left") {
+			this.upMove();
+			this.LeftMove();
+		}
+        if(direction1 == "up" && direction2 == "right") {
+			this.upMove();
+			this.RightMove();
+		}
+        if(direction1 == "down" && direction2 == "left") {
+			this.downMove();
+			this.LeftMove();
+		} 
+        if(direction1 == "down" && direction2 == "right") {
+			this.downMove();
+			this.RightMove();
+		} 
 		
 	}
 	public void level2Move() {
