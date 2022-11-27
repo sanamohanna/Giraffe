6a3b4fdaf451759bd4688c84b8e2c031947cf802
klarahanany <klarosh.hanany@gmail.com>
2022-11-27 22:20:47 +0200

    DiagonallyMove method

--- Knight'sMove/src/model/Board.java
+++ Knight'sMove/src/model/Board.java
@@ -2,11 +2,23 @@ package model;
 
 public class Board {
 	private Squares[][] boxes;
-
+	private static Board instance=null;
 	public Board(Squares[][] boxes) {
 		super();
 		this.boxes = boxes;
 	}
+	public Board() {
+		super();
+	}
+	//Get the only object available
+		public static Board getInstance(){
+			
+			if (instance == null) 
+			{ 
+				instance = new Board(); 
+			} 
+			return instance; 
+		}
 
 	public Squares[][] getBoxes() {
 		return boxes;
