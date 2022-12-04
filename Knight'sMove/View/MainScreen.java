a5a1da1e8e9b1acb7c7ffc52e20aed448088724b
klarahanany <klarosh.hanany@gmail.com>
2022-12-04 15:24:12 +0200

    commit

---
+++ Knight'sMove/View/MainScreen.java
@@ -0,0 +1,36 @@
+package View;
+
+import java.io.IOException;
+
+import Control.QuestionMngController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+
+public class MainScreen extends Application {
+
+	public void start(Stage primaryStage) throws  IOException{
+		
+		try {
+
+			Parent root = FXMLLoader.load(getClass().getResource("/View/MainScreen.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("/View/mainScreen.css").toExternalForm());
+			Image icon = new Image("6808582.png");
+			primaryStage.setResizable(false);
+			primaryStage.getIcons().add(icon);
+			primaryStage.setTitle("Knight's Move Game");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		catch (IOException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+}
