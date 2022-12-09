9dbd7de9a2ded4deba853b9d1c943b5b3b2782e7
sanamohanna <sanamohanna2305@gmail.com>
2022-12-09 14:38:16 +0200

    .

--- Knight'sMove/src/Control/MainScreenController.java
+++ Knight'sMove/src/Control/MainScreenController.java
@@ -2,15 +2,15 @@ package Control;
 
 import java.io.IOException;
 
-import View.MainScreen;
+//import View.MainScreen;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+//import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class MainScreenController {
--- Knight'sMove/src/Control/QuestionMngController.java
+++ Knight'sMove/src/Control/QuestionMngController.java
@@ -20,7 +20,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
+//import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.AdminPlayer;
