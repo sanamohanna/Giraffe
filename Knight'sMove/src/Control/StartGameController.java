7f4ee8b08fa1e25c4780cf9edffd0e1456eaebb9
klarahanany <klarosh.hanany@gmail.com>
2022-12-11 13:48:27 +0200

    commit

--- Knight'sMove/src/Control/StartGameController.java
+++ Knight'sMove/src/Control/StartGameController.java
@@ -1,14 +1,22 @@
 package Control;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class StartGameController implements Initializable {
 	
@@ -18,11 +26,22 @@ public class StartGameController implements Initializable {
 	private Text text;
 	@FXML
 	private Text level;
-	
+	@FXML
+	private Text pointsT;
 	
 	//timer fields;
-	static long min,hr, sec,totalSec;
+	static long min,hr, sec,totalSec,points=0;
+	public void backButton(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/View/MainScreen.fxml"));
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setResizable(false);
+		scene.getStylesheets().add(getClass().getResource("/View/mainScreen.css").toExternalForm());
+		stage.setScene(scene);
 	
+		stage.show();
+
+	}
 	
 	public void setTimer() {
 		totalSec=60;
@@ -32,6 +51,8 @@ public class StartGameController implements Initializable {
 			@Override
 			public void run() {
 				convertTime();
+				points++;
+				pointsT.setText("POINTS:"+String.valueOf(points));
 				if(totalSec<=0) {	
 				text.setText("00:00");
 				timer.cancel();
