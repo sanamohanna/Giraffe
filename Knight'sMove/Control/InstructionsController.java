886350cd992fb77a61db219d2ff31984199564b6
Safa Bader <safa.bader28@hotmail.com>
2022-12-11 18:39:38 +0200

    ...

---
+++ Knight'sMove/Control/InstructionsController.java
@@ -0,0 +1,27 @@
+package Control;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+// Event Listener on Button[#backButton].onAction
+public class InstructionsController {
+@FXML
+private Button backButton;
+
+public void backButton(ActionEvent event) throws IOException {
+	Parent root = FXMLLoader.load(getClass().getResource("/View/MainScreen.fxml"));
+	Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	Scene scene = new Scene(root);
+	// stage.setResizable(false);
+	scene.getStylesheets().add(getClass().getResource("/View/mainScreen.css").toExternalForm());
+	stage.setScene(scene);
+	stage.show();
+
+	}
+}
