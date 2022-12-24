c5c8980c59f0e91061dea9c947a37f4396396b9d
Safa Bader <safa.bader28@hotmail.com>
2022-12-24 23:41:59 +0200

    Update KnightMoveTest.java

--- Knight'sMove/src/UnitTests/KnightMoveTest.java
+++ Knight'sMove/src/UnitTests/KnightMoveTest.java
@@ -13,23 +13,39 @@ import model.Location;
 class KnightMoveTest {
 
 	@Test
-	void level1Test() {
+	void level1KnightMoveRRDTest() {
 		Knight knight = new Knight(new Location(0, 0));
-		Location c = knight.level1Move(Directions.RIGHT, Directions.RIGHT, Directions.DOWN, "0", "0");
-		Location locationExpected = new Location(2, 1);
-		assertEquals(locationExpected, c);
+		Location actualLocation = knight.level1Move(Directions.RIGHT, Directions.RIGHT, Directions.DOWN, "0", "0");
+		Location expectedLocation = new Location(2, 1);
+		assertEquals(expectedLocation, actualLocation);
 	}
 
 	@Test
-	void level2Test() {
+	void level1KnightMoveRRUTest() {
 		Knight knight = new Knight(new Location(0, 0));
-		Location c = knight.level2Move(Directions.RIGHT, Directions.RIGHT, Directions.DOWN_RIGHT, "0", "0");
-		Location locationExpected = new Location(3, 1);
-		assertEquals(locationExpected, c);
+		Location actualLocation = knight.level1Move(Directions.RIGHT, Directions.RIGHT, Directions.UP, "0", "1");
+		Location expectedLocation = new Location(2, 0);
+		assertEquals(expectedLocation, actualLocation);
 	}
 
 	@Test
-	void allValidMoveLevel1Test() {
+	void level2KnightMoveRRD_RTest() {
+		Knight knight = new Knight(new Location(0, 0));
+		Location actualLocation = knight.level2Move(Directions.RIGHT, Directions.RIGHT, Directions.DOWN_RIGHT, "0", "0");
+		Location expectedLocation = new Location(3, 1);
+		assertEquals(expectedLocation, actualLocation);
+	}
+
+	@Test
+	void level2KnightMoveRRU_RTest() {
+		Knight knight = new Knight(new Location(0, 0));
+		Location actualLocation = knight.level2Move(Directions.RIGHT, Directions.RIGHT, Directions.UP_RIGHT, "0", "0");
+		Location expectedLocation = new Location(3, 7);
+		assertEquals(expectedLocation, actualLocation);
+	}
+
+	@Test
+	void knightAllValidMoveLevel1Test() {
 		/*
 		 * 0,3 (up up right)
 		 * 0,1 (up up left )
@@ -43,22 +59,21 @@ class KnightMoveTest {
 		 */
 		Knight knight = new Knight(new Location(2, 2));
 		ArrayList<Location> actualLocations = knight.allValidMovesLevel1(knight);
-		ArrayList<Location> locationExpected = new ArrayList<Location>();
+		ArrayList<Location> expectedLocations = new ArrayList<Location>();
 		for (int i = 0; i < actualLocations.size(); i++) {
 			System.out.println("the actual:" + actualLocations.get(i));
 		}
-		locationExpected.add(new Location(1, 4));
-		locationExpected.add(new Location(3, 4));
-		locationExpected.add(new Location(1, 0));
-		locationExpected.add(new Location(3, 0));
-		locationExpected.add(new Location(4, 3));
-		locationExpected.add(new Location(4, 1));
-		locationExpected.add(new Location(0, 3));
-		locationExpected.add(new Location(0, 1));
-		for (int i = 0; i < locationExpected.size(); i++) {
-			System.out.println("the expected:" + locationExpected.get(i));
+		expectedLocations.add(new Location(1, 4));
+		expectedLocations.add(new Location(3, 4));
+		expectedLocations.add(new Location(1, 0));
+		expectedLocations.add(new Location(3, 0));
+		expectedLocations.add(new Location(4, 3));
+		expectedLocations.add(new Location(4, 1));
+		expectedLocations.add(new Location(0, 3));
+		expectedLocations.add(new Location(0, 1));
+		for (int i = 0; i < expectedLocations.size(); i++) {
+			System.out.println("the expected:" + expectedLocations.get(i));
 		}
-		assertEquals(locationExpected, actualLocations);
+		assertEquals(expectedLocations, actualLocations);
 	}
-
-}
+	}
