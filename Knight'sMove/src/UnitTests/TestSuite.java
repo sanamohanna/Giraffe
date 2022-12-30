2e87ee6e17e67cb0f2042fa0ec2b4e4a19548da9
Safa Bader <safa.bader28@hotmail.com>
2022-12-30 03:56:09 +0200

    Create TestSuite.java

---
+++ Knight'sMove/src/UnitTests/TestSuite.java
@@ -0,0 +1,11 @@
+package UnitTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+	KnightMoveTest.class,
+	QueenMoveTestTest.class })
+public class TestSuite {
+}
